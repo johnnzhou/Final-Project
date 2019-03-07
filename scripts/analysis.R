4a571402f4a0748d1f4cdee155230ea76ee19058
Zhennan Zhou <zhouz46@uw.edu>
2019-03-06 23:06:04 -0800

    Update scripts

---
+++ scripts/analysis.R
@@ -0,0 +1,9 @@
+install.packages("dplyr")
+install.packages("tidyr")
+install.packages("ggplot2")
+install.packages("plotly")
+
+library("dplyr")
+library("tidyr")
+library("ggplot2")
+library("plotly")
