a034b32ba8baa4d1354a81eb9335048dc1a2560c
Zhennan Zhou <zhouz46@uw.edu>
2019-03-06 23:11:32 -0800

    Update the UI framework

--- scripts/ui.R
+++ scripts/ui.R
@@ -11,23 +11,61 @@ library(shiny)
 
 # Define UI for application that draws a histogram
 shinyUI(fluidPage(
-  
-  # Application title
-  titlePanel("Old Faithful Geyser Data"),
-  
-  # Sidebar with a slider input for number of bins 
-  sidebarLayout(
-    sidebarPanel(
-       sliderInput("bins",
-                   "Number of bins:",
-                   min = 1,
-                   max = 50,
-                   value = 30)
+    "Gender and Salary",
+    
+    # tab 1
+    tabPanel(
+        "",
+        titlePanel(""),
+        sidebarLayout(
+            sidebarPanel(
+                sliderInput()
+            ),
+            mainPanel(
+                plotOutput("")
+            )
+        )
     ),
     
-    # Show a plot of the generated distribution
-    mainPanel(
-       plotOutput("distPlot")
+    # tab 2
+    tabPanel(
+        "",
+        titlePanel(""),
+        sidebarLayout(
+            sidebarPanel(
+                sliderInput()
+            ),
+            mainPanel(
+                plotOutput("")
+            )
+        )
+    ),
+    
+    # tab 3
+    tabPanel(
+        "",
+        titlePanel(""),
+        sidebarLayout(
+            sidebarPanel(
+                sliderInput()
+            ),
+            mainPanel(
+                plotOutput("")
+            )
+        )
+    ),   
+    
+    # tab4
+    tabPanel(
+        "",
+        titlePanel(""),
+        sidebarLayout(
+            sidebarPanel(
+                sliderInput()
+            ),
+            mainPanel(
+                plotOutput("")
+            )
+        )
     )
-  )
 ))
